872c63fdce0de96381ab6a7bd67cef82bdeb257e
Angel-Gab <angele.gaboriau@isen-ouest.yncrea.fr>
2020-05-13 17:38:04 +0200

    TP14

--- Exercice3.py
+++ Exercice3.py
@@ -10,21 +10,7 @@ class Curseur (QWidget) :
         layout = QVBoxLayout()
 
         self.setWindowTitle("IHM")
-        self.myicon = QIcon("fr-flag.png")
-        self.setWindowIcon(self.myicon)
-        layout = QVBoxLayout()
-        label = QLabel("Hello World")
-        label.setAlignment(Qt.AlignCenter)
-        progressbar = QProgressBar()
-        progressbar.setValue(50)
-        lineedit = QLineEdit()
-        clickme = QPushButton("Click me !")
-        clickme.setToolTip("PLEASE")
-        layout.addWidget(label)
-        layout.addWidget(progressbar)
-        layout.addWidget(lineedit)
-        layout.addWidget(clickme)
-        self.setLayout(layout)
+
 
 
 
